c758571ffe2ea325a019c5796f65a8ea8e880bb6
Tdiggs01 <tyrei1diggs2@csu.fullerton.edu>
2024-02-27 11:38:33 -0800

    Completed both algorithms

--- Lawnmower Algorithm/Lawnmower.py
+++ Lawnmower Algorithm/Lawnmower.py
@@ -1,1 +1,39 @@
+# Tyrei Diggs 2/26/2024
+# CPSC 335 Project 1
+# Purpose: Create an alternating algorithm that separates 1's and 0's
+
+def lawnmower(size, disk):
+    """Algorithm sorts the array like a lawnmower cuts grass."""
+    for x in range(int(size / 2)-1):
+        for i in range(size-1):
+            if disk[i] > disk[i + 1]:
+                temp = disk[i]
+                disk[i] = disk[i + 1]
+                disk[i + 1] = temp
+        print(disk)
+        for j in range(size-1, 0, -1):
+            if disk[j] < disk[j-1]:
+                temp = disk[j]
+                disk[j] = disk[j-1]
+                disk[j-1] = temp
+        print(disk)
+    return disk
+
+
+def main():
+    """ Create an array of Zeros and ones."""
+    n = int(input("Input positive number for number of zeros/ones:"))
+    zeros, ones = n, n
+    disk = []
+
+    for i in range(n):
+        disk.append("1")
+        disk.append("0")
+    print(disk)
+
+    print("Sorting this array of ones and zeros using the lawnmower algorithm.")
+    lawnmower(2*n, disk)
+    print(disk)
+
+main()
 
--- Left-to-Right-Algorithm/Left-toRight.py
+++ Left-to-Right-Algorithm/Left-toRight.py
@@ -1,1 +1,34 @@
+# Tyrei Diggs 2/26/2024
+# CPSC 335 Project 1
+# Purpose: Create an alternating algorithm that separates 1's and 0's
+
+def leftRight(size, disk):
+    """Algorithm goes from font end to back end until ones and zeros are separated."""
+    for i in range(size):
+    # Loop the same number of time as the size of the array
+        for j in range(size-1):
+        # starts at begining of array and goes to end
+            if disk[j] > disk[j+1]:
+                temp = disk[j]
+                disk[j] = disk[j+1]
+                disk[j+1] = temp
+    return disk
+
+
+def main():
+    """ Create an array of Zeros and ones."""
+    n = int(input("Input positive number for number of zeros/ones:"))
+    zeros, ones = n, n
+    disk = []
+
+    for i in range(n):
+        disk.append("1")
+        disk.append("0")
+    print(disk)
+
+    print("Sorting this array of ones and zeros using the left-to-right algorithm.")
+    leftRight(2*n, disk)
+    print(disk)
+
+main()
 
